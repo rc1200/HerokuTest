dbf24ac678551bc6db70ab578928011308686b5d
rc1200 <ron.calibuso@gmail.com>
2019-02-21 18:57:44 -0500

    made some chagnes... at hello x times

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
@@ -8,9 +9,8 @@ from .models import Greeting
 
 
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
 
 
 def db(request):
