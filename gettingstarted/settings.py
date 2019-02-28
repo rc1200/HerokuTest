730fe93a532a0bfdd3874b2bf036f543c00e1088
rc1200 <ron.calibuso@gmail.com>
2019-02-28 12:36:43 -0500

    migrated sqllite to postgres then changed sqlite name to ensure it is not connected, attemp to push to heroku

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'herokutestdb',
+            'USER': 'postgres',
+            'PASSWORD': 'herokutest',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
 }
 
 # Password validation
